6bbe60e6f0fe143affe49de1cb7757ed54da03a8
Ahmed Dif <ahmed.def.g@gmail.com>
2020-03-01 21:43:38 +0300

    project requirements

---
+++ src/app/my-project/project-requirements/project-requirements.component.ts
@@ -0,0 +1,116 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ProjectModel} from '../../shared/models/project.model';
+import {ProjectService} from '../../shared/services/project.service';
+import {Subscription} from 'rxjs';
+import {ProjectRequirementModel} from '../../shared/models/projectRequirement.model';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ToastrService} from 'ngx-toastr';
+import * as _ from 'lodash';
+import swal from 'sweetalert2';
+
+
+@Component({
+    selector: 'app-project-requirements',
+    templateUrl: './project-requirements.component.html',
+    styleUrls: ['./project-requirements.component.scss']
+})
+export class ProjectRequirementsComponent implements OnInit, OnDestroy {
+    @Input() currentProject: ProjectModel;
+    projectRequirementsSubscription: Subscription;
+    projectRequirementSubscription: Subscription;
+    projectRequirements: ProjectRequirementModel[];
+    reqDesc;
+    reqPriority;
+    reqId;
+    public RangeConfig: any = {
+        behaviour: 'drag',
+        connect: true,
+        step: 1,
+        range: {
+            min: 1,
+            max: 10
+        },
+        pips: {
+            mode: 'steps'
+        }
+    };
+
+    constructor(public projectService: ProjectService, private loaderService: NgxSpinnerService,
+                private modalService: NgbModal, private toastr: ToastrService) {
+    }
+
+    ngOnInit() {
+        this.loaderService.show();
+        this.projectRequirementsSubscription = this.projectService.projectRequirementsSubject.subscribe(reqs => {
+            this.projectRequirements = reqs;
+            this.loaderService.hide();
+        });
+        this.projectService.loadProjectRequirements(this.currentProject.projectId);
+    }
+
+
+    onAddRequirement(content) {
+        this.reqId = 0;
+        this.reqDesc = '';
+        this.reqPriority = 5;
+        this.modalService.open(content).result.then((result) => {
+            if (_.isEmpty(this.reqDesc)) {
+                this.toastr.error('Please enter requirement description.')
+            } else {
+                this.onSaveReq();
+            }
+        }, (reason) => {
+        });
+    }
+
+
+    onSaveReq() {
+        this.loaderService.show();
+        this.projectRequirementSubscription = this.projectService.projectRequirementSubject.subscribe(req => {
+            this.loaderService.hide();
+        });
+        this.projectService.saveProjectRequirement({
+            reqId: this.reqId,
+            reqDesc: this.reqDesc,
+            reqPriority: this.reqPriority,
+            projectId: this.currentProject.projectId
+        });
+    }
+
+    onEditRequirement(req: ProjectRequirementModel, content) {
+        this.reqId = req.reqId;
+        this.reqDesc = req.reqDesc;
+        this.reqPriority = req.reqPriority;
+        this.modalService.open(content).result.then((result) => {
+            if (_.isEmpty(this.reqDesc)) {
+                this.toastr.error('Please enter requirement description.')
+            } else {
+                this.onSaveReq();
+            }
+        }, (reason) => {
+        });
+    }
+
+    onDeleteAction(req: ProjectRequirementModel) {
+        swal.fire({
+            title: 'Detele Requirement',
+            text: 'Do you want to delete this requirement?',
+            type: 'warning',
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'Cancel',
+            preConfirm: () => {
+                this.projectService.deleteRequirement(req);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        if (this.projectRequirementsSubscription) {
+            this.projectRequirementsSubscription.unsubscribe();
+        }
+        if (this.projectRequirementSubscription) {
+            this.projectRequirementSubscription.unsubscribe();
+        }
+    }
+}
---
+++ src/app/shared/services/project.service.ts
@@ -0,0 +1,97 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
+import {ProjectTypeModel} from '../models/projectType.model';
+import {Subject} from 'rxjs';
+import {activeLink} from './backend-link';
+import {ProjectModel} from '../models/project.model';
+import {ProjectRequirementModel} from '../models/projectRequirement.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProjectService {
+    projectTypeSubject: Subject<ProjectTypeModel[]> = new Subject<ProjectTypeModel[]>()
+    MyProjectTypeSubject: Subject<ProjectModel[]> = new Subject<ProjectModel[]>()
+    addProjectSubject: Subject<ProjectModel> = new Subject<ProjectModel>();
+    projectRequirementsSubject: Subject<ProjectRequirementModel[]> = new Subject<ProjectRequirementModel[]>();
+    projectRequirementSubject: Subject<ProjectRequirementModel> = new Subject<ProjectRequirementModel>();
+    currentSelectedProjectId: number;
+
+    constructor(private http: HttpClient, private toastr: ToastrService) {
+    }
+
+    loadProjectById(projectId) {
+        console.log(projectId);
+        this.http.get<ProjectModel>(activeLink[0] + '/project/auth/' + projectId).subscribe(project => {
+            this.addProjectSubject.next(project);
+        }, error => {
+            this.toastr.error(error.message);
+            this.addProjectSubject.next(null);
+        });
+    }
+
+
+    loadMyProjects() {
+        this.http.get<ProjectModel[]>(activeLink[0] + '/project/auth/all').subscribe(projcets => {
+            this.MyProjectTypeSubject.next(projcets)
+        }, error => {
+            this.toastr.error(error.message);
+            this.MyProjectTypeSubject.next(null);
+        });
+    }
+
+    loadProjectTypes() {
+        this.http.get<ProjectTypeModel[]>(activeLink[0] + '/project/auth/types').subscribe(types => {
+            this.projectTypeSubject.next(types);
+        }, error => {
+            this.toastr.error(error.message);
+            this.projectTypeSubject.next(null);
+        });
+    }
+
+    setProjectData(project: ProjectModel) {
+        this.http.post<ProjectModel>(activeLink[0] + '/project/auth/set', project).subscribe(projectOb => {
+            this.addProjectSubject.next(projectOb);
+            this.toastr.success('Project saved successfully.');
+        }, error => {
+            this.toastr.error(error.message);
+            this.addProjectSubject.next(null);
+        });
+    }
+
+    loadProjectRequirements(projectId) {
+        this.http.get<ProjectRequirementModel[]>(activeLink[0] + '/project/auth/req/' + projectId).subscribe((reqs) => {
+            this.projectRequirementsSubject.next(reqs);
+        }, error => {
+            this.toastr.error(error.message);
+            this.projectRequirementsSubject.next(null);
+        });
+    }
+
+    saveProjectRequirement(projectReq: ProjectRequirementModel) {
+        this.http.post<ProjectRequirementModel>(activeLink[0] + '/project/auth/setReq', projectReq).subscribe(req => {
+            this.projectRequirementSubject.next(req);
+            this.toastr.success('Project requirement saved.');
+            this.loadProjectRequirements(req.projectId);
+            this.loadProjectById(req.projectId);
+        }, error => {
+            this.toastr.error(error.message);
+            this.projectRequirementSubject.next(null);
+        });
+    }
+
+    deleteRequirement(req: ProjectRequirementModel) {
+        this.http.delete(activeLink[0] + '/project/auth/delReq/' + req.reqId).subscribe(req1 => {
+            if (req1) {
+                this.toastr.success('Requirement deleted successfully.');
+            }
+            this.loadProjectRequirements(req.projectId);
+            this.loadProjectById(req.projectId);
+        }, error => {
+            this.toastr.error(error.message)
+            console.log(error);
+        });
+    }
+
+}
